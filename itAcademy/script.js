21add48c4dbaef6b843245df474a4e58b7de8faf
miguel-warken <128272042+miguel-warken@users.noreply.github.com>
2024-03-14 16:31:12 -0300

    Esqueleto de métodos no JS e de elementos no HTML
    Adicionado o esqueleto de métodos que serão executados pelo JavaScript no pressionar de um botão. Adicionado o ícone da aba. Adicionado elementos no HTML de botões e caixas de inserção de texto/números.

--- itAcademy/script.js
+++ itAcademy/script.js
@@ -3,4 +3,12 @@
 var betting, drawing, counting, rewarding;
 var bets = [];
 
+start = function () {};
+
 makeBet = function () {};
+
+listBets = function () {};
+
+roll = function () {};
+
+showResults = function () {};
