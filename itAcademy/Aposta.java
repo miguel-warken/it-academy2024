c39277b9c98a0dbe13d39977602fa222310e14cc
miguel-warken <128272042+miguel-warken@users.noreply.github.com>
2024-03-16 14:35:41 -0300

    Funcionalidade de Registrar Apostas completa
    Completada a função de registrar apostas, adicionando opções para escolher números ou surpresinha e colocando no registro de cada pessoa suas apostas, se seu CPF já tiver sido cadastrado.

--- itAcademy/Aposta.java
+++ itAcademy/Aposta.java
@@ -1,12 +1,19 @@
-import java.util.List;
 import java.util.ArrayList;
 
-public class Aposta{
+public class Aposta {
     int id;
     ArrayList<Integer> numerosAposta;
-    
-    public Aposta(int id, ArrayList<Integer> numerosAposta){
+
+    public Aposta(int id, ArrayList<Integer> numerosAposta) {
         this.id = id;
         this.numerosAposta = numerosAposta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ArrayList<Integer> getNumerosAposta() {
+        return numerosAposta;
     }
 }
--- itAcademy/Pessoa.java
+++ itAcademy/Pessoa.java
@@ -1,27 +1,30 @@
-import java.util.List;
 import java.util.ArrayList;
 
-public class Pessoa{
+public class Pessoa {
     private String nome;
     private String cpf;
     private ArrayList<Aposta> apostas;
-    
-    public Pessoa(String cpf, String nome, Aposta aposta){
+
+    public Pessoa(String cpf, String nome, Aposta aposta) {
         this.cpf = cpf;
         this.nome = nome;
         this.apostas = new ArrayList<>();
         apostas.add(aposta);
     }
-    
-    public void adicionaApostaPessoaExistente(Aposta aposta){
+
+    public void adicionaApostaPessoaExistente(Aposta aposta) {
         apostas.add(aposta);
     }
-    
-    public String getNome(){
+
+    public String getNome() {
         return this.nome;
     }
-    
-    public String getCpf(){
+
+    public String getCpf() {
         return this.cpf;
+    }
+
+    public ArrayList<Aposta> getApostas() {
+        return apostas;
     }
 }
